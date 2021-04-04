7481cf22eb1896982d87af397a8e11c72b07b5ec
sharifahmed <sa6304659@gmail.com>
2021-04-04 14:56:59 +0600

    I added owl carousel in slider

--- js/main.js
+++ js/main.js
@@ -27,67 +27,74 @@ $(window).on('scroll', function () {
 
 
 // mainSlider
-function mainSlider() {
-	var BasicSlider = $('.slider-active');
-	BasicSlider.on('init', function (e, slick) {
-		var $firstAnimatingElements = $('.single-slider:first-child').find('[data-animation]');
-		doAnimations($firstAnimatingElements);
-	});
-	BasicSlider.on('beforeChange', function (e, slick, currentSlide, nextSlide) {
-		var $animatingElements = $('.single-slider[data-slick-index="' + nextSlide + '"]').find('[data-animation]');
-		doAnimations($animatingElements);
-	});
-	BasicSlider.slick({
-		autoplay: true,
-		dots:true,
-		autoplaySpeed: 1000,
-		dots: false,
-		fade: true,
-		arrows: false,
-		responsive: [
-			{ breakpoint: 767, settings: { dots: false, arrows: false,
-			autoplay:true,
-			dots:true
-		} }
-		]
-	});
+// function mainSlider() {
+// 	var BasicSlider = $('.slider-active');
+// 	BasicSlider.on('init', function (e, slick) {
+// 		var $firstAnimatingElements = $('.single-slider:first-child').find('[data-animation]');
+// 		doAnimations($firstAnimatingElements);
+// 	});
+// 	BasicSlider.on('beforeChange', function (e, slick, currentSlide, nextSlide) {
+// 		var $animatingElements = $('.single-slider[data-slick-index="' + nextSlide + '"]').find('[data-animation]');
+// 		doAnimations($animatingElements);
+// 	});
+// 	BasicSlider.slick({
+// 		autoplay: true,
+// 		dots:true,
+// 		autoplaySpeed: 1000,
+// 		dots: false,
+// 		fade: true,
+// 		arrows: false,
+// 		responsive: [
+// 			{ breakpoint: 767, settings: { dots: false, arrows: false,
+// 			autoplay:true,
+// 			dots:true
+// 		} }
+// 		]
+// 	});
 
-	function doAnimations(elements) {
-		var animationEndEvents = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
-		elements.each(function () {
-			var $this = $(this);
-			var $animationDelay = $this.data('delay');
-			var $animationType = 'animated ' + $this.data('animation');
-			$this.css({
-				'animation-delay': $animationDelay,
-				'-webkit-animation-delay': $animationDelay
-			});
-			$this.addClass($animationType).one(animationEndEvents, function () {
-				$this.removeClass($animationType);
-			});
-		});
-	}
-}
-mainSlider();
+// 	function doAnimations(elements) {
+// 		var animationEndEvents = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+// 		elements.each(function () {
+// 			var $this = $(this);
+// 			var $animationDelay = $this.data('delay');
+// 			var $animationType = 'animated ' + $this.data('animation');
+// 			$this.css({
+// 				'animation-delay': $animationDelay,
+// 				'-webkit-animation-delay': $animationDelay
+// 			});
+// 			$this.addClass($animationType).one(animationEndEvents, function () {
+// 				$this.removeClass($animationType);
+// 			});
+// 		});
+// 	}
+// }
+// mainSlider();
 
 
 // owlCarousel
 $('.owl-carousel').owlCarousel({
     loop:true,
     margin:0,
-	items:1,
+	items:3,
 	navText:['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
     nav:true,
-	dots:false,
+	dots:true,
+	autoplay:true,
+	autoplaySpeed:1000,
+	animationIn:true,
+	animationOut:true,
+
+	
     responsive:{
         0:{
             items:1
         },
         767:{
-            items:3
+            items:1
         },
         992:{
-            items:5
+            items:1,
+			dots:true
         }
     }
 })
